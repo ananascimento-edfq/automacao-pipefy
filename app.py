375fd8deb26cabf8c5aa98b4fff5ad8bf57813a4
ananascimento-edfq <ana.nascimento@edifiquejr.com>
2026-03-25 00:24:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,29 @@
 import requests
 
-# 🔹 CONFIGURAÇÕES (vamos preencher depois)
-PIPEFY_TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJQaXBlZnkiLCJpYXQiOjE3NzQzOTYyMTgsImp0aSI6ImFjOWE3OGNlLTk4NjUtNDY4MC04MWE2LWY2ODc1NGQ3NmNlMiIsInN1YiI6MzA2NzU2NDE5LCJ1c2VyIjp7ImlkIjozMDY3NTY0MTksImVtYWlsIjoiYW5hLm5hc2NpbWVudG9AZWRpZmlxdWVqci5jb20ifSwidXNlcl90eXBlIjoiYXV0aGVudGljYXRlZCJ9.2VY-"
+PIPEFY_TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJQaXBlZnkiLCJpYXQiOjE3NzQ0MDkwMjksImp0aSI6IjdmNjdkMTI2LTYxYWMtNGQ1Zi04M2Y1LTcxOWNkZGI1NGU2OCIsInN1YiI6MzA2NzU2NDE5LCJ1c2VyIjp7ImlkIjozMDY3NTY0MTksImVtYWlsIjoiYW5hLm5hc2NpbWVudG9AZWRpZmlxdWVqci5jb20ifSwidXNlcl90eXBlIjoiYXV0aGVudGljYXRlZCJ9.2rfii5au0v12Pj6Kb2YkuNOzw2RiMR_qMOLkKT9zTsBSGspf7fDCuY1AsaFfy7gF74Xlt0f-OwXu9UMofQL19A"
 PIPE_ID = "306967557"
 
-def criar_card(nome, email, resposta):
-    url = "https://api.pipefy.com/graphql"
+url = "https://api.pipefy.com/graphql"
 
-    query = f'''
+query = f"""
 mutation {{
   createCard(input: {{
     pipe_id: {PIPE_ID},
-    fields_attributes: [
-      {{"field_id": "nome", "field_value": nome}},
-      {{"field_id": "email", "field_value": email}},
-      {{"field_id": "resposta", "field_value": resposta}}
-    ]
+    title: "Teste automático"
   }}) {{
     card {{
       id
+      title
     }}
   }}
 }}
-'''
+"""
 
-    headers = {
-    "Authorization": f"Bearer {PIPEFY_TOKEN}",
+headers = {{
+    "Authorization": f"Bearer {{PIPEFY_TOKEN}}",
     "Content-Type": "application/json"
-}
+}}
 
-    response = requests.post(url, json={"query": query}, headers=headers)
+response = requests.post(url, json={{"query": query}}, headers=headers)
 
 print(response.text)
-
-
-# 🔹 TESTE
-criar_card("Teste automático", "teste@email.com", "Funcionou 🚀")
-# teste
